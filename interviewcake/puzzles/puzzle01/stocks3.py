0d81f158b1ca6c3f38cee82eba92d05152aa8f64
gigatexal <gigatexal@workstation.alex@alex>
2016-02-12 13:32:02 -0800

    small fixes

--- interviewcake/puzzles/puzzle01/stocks3.py
+++ interviewcake/puzzles/puzzle01/stocks3.py
@@ -1,10 +1,15 @@
-import random
+'''
+   simple program, if you could look back in time and buy a stock and sell it, what's the 
+   max amount you could make?
+   Input: a 1 x n array / list of stock prices where index position after index 0 denotes
+          time after open, open being 9:30EST. 
+   Output: Maximum profit. 
+'''
 
-#stocks = [10, 7, 5, 8, 11, 9]
-stocks = [random.randint(1,20) for x in range(0,100000001)]
+stocks = [10, 7, 5, 8, 11, 9]
 
 
-currLow = 99999999
+currLow = 99999999 #arbitrary to make the logic work, fixme?
 currProfit = 0
 for i in range(0,len(stocks)):
    if currLow > stocks[i]:
