be0fcfbf463778d7d9109fb73ad1e2f41ee6dfbb
Alex Narayan <alex@alexandarnarayan.com>
2016-06-15 11:45:12 -0700

    close, but not there

--- CS162/prototyping/mrg.py
+++ CS162/prototyping/mrg.py
@@ -3,13 +3,33 @@ import random as rnd
 unsorted_arr = [3,4,9,8,2,1,7]
 
 def _mid(lst):
-   return int(len(lst)/2)
+   arr_len = len(lst)
+   if lst is not None and arr_len > 0:
+      return int(arr_len/2)
+   if arr_len == 1:
+      return 1
+   #else returns None      
 
-r = []
-print(_mid(r))
-for i in range(50):
-   arr = [x for x in range(0,int(rnd.random()*10)+1)]
-   print(len(arr))
-   print("midpoint is ",_mid(arr))
+def _split(lst):
+   midp = _mid(lst)
+   left,right = [],[]
+   left,right = lst[0:midp],lst[midp:]
+   return (left,right)
+
+def _merge(left,right):
+   combined = []
+   for elem in left:
+      combined.append(elem)
+   for elem in right:
+      combined.append(elem)
+   return combined
+
+def mrgSort(lst):
+   srtd = []
+   unsorted = lst;
+   pivot = unsorted.pop(_mid(unsorted))
+   left,right = _split(unsorted)
+   return left,right,pivot,srtd
 
 
+print (mrgSort(unsorted_arr) )
