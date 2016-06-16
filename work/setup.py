f0826833f35f51aeb26937ed3c96a58479a51790
alex n <alex@alexandarnarayan.com>
2016-06-16 15:14:56 -0700

    commit from windows

--- work/setup.py
+++ work/setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['fixpadding.py'])
+setup(console=['C:\\Users\\alexandar_narayan\\PycharmProjects\\languagestudy\\languagestudy\\work\\fixpadding.py'],
+      options = {
+                "py2exe":{
+                         "includes":['pyreadline','win32evtlog', 'win32evtlogutil']
+                         }
+      }      
+)
