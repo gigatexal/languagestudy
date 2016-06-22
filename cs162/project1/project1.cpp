a3657ae7be4e2114a9dde4d808735ce640a2541d
Alex Narayan <alex@alexandarnarayan.com>
2016-06-21 19:33:33 -0700

    refactor to not use <string>

--- cs162/project1/project1.cpp
+++ cs162/project1/project1.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
+#include <climits>
 using namespace std;
 
 void display_output(float money);
+string get_item();
+float get_cost(string item);
+float get_cost();
+void clear_input_buffer();
 
 int main(){
-   string item;
+   
    char user_input;
    float cost;
+   float running_total;
+
    while (true){
-     cout << "Enter in the item: ";
-     getline(cin,item);
-     cout << "Enter in the price: ";
-     cin >> cost;
-     cout << "continue ? y - to continue, n - to exit: " << endl;
+     item = get_item();
+     cout << "Item entered: " << item << endl;
+     cost = get_cost(item);
+     cout << "Cost entered: " << cost << endl;
+     cout << "Do you want to continue? type y - to continue, n - to exit: ";
      cin  >> user_input;
      if (user_input == 'n' || user_input == 'N'){
         break;
      }
    }
-
+   
    return 0;
 }
 
@@ -30,4 +36,25 @@ void display_output(float money){
    cout.setf(ios::showpoint);
    cout.precision(2);
    cout << money << endl;
+}
+
+void clear_input_buffer(){
+   cin.clear();
+   cin.ignore(CHAR_MAX,'\n');
+}
+
+string get_item(){
+   string input;
+   cout << "Enter in the item name e.g Apples: ";
+   getline(cin,input);
+   return input;
+}
+
+float get_cost(string item){
+   float cost;
+   cout << "Enter in the price of :";
+   cin >> cost;
+   while(!cin){
+      get_cost();
+   return cost;
 }
