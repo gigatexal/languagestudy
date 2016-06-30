b5d899fd20d905f984b2c5e8cf6d98da98ed5c74
Alex Narayan <alex@alexandarnarayan.com>
2016-06-29 23:50:06 -0700

    ahh better

--- work/next/fixpadding.py
+++ work/next/fixpadding.py
@@ -28,13 +28,26 @@ def combine_iterators(*args):
    yield from itertools.chain(args)
   
 
-def replace_in_file(file_to_edit,replace_this,with_this):
+def _replace_in_file(file_to_edit,replace_this,with_this):
    with open(file_to_edit,'r') as curr_file:
       contents = curr_file.read()
       contents = contents.replace(replace_this,with_this)
    
    with open(file_to_edit,'w+') as curr_file:
       curr_file.write(contents)  
+
+def _get_file_hash(file):
+   with open(file,'r') as curr_file:
+      return hash(curr_file.read())
+
+def replace_in_files(file_to_edit,replace_this,with_this):
+   before_hash    = _get_file_hash(file_to_edit)
+   _replace_in_file(file_to_edit,replace_this,with_this)
+   after_hash     = _get_file_hash(file_to_edit)
+   if before_hash == after_hash:
+      return False
+   else:
+      return True
 
 
 if __name__ == '__main__':
@@ -48,12 +61,12 @@ if __name__ == '__main__':
    files             = itertools.chain(dirs,files_from_cli)
    replication_files = (file for file in files if os.path.splitext(file)[1] == '.sch')
    
-   num_files = 0
+   num_files_modified = 0
    for file in replication_files:
-      replace_in_file(file,"SET ANSI PADDING OFF","SET ANSI PADDING ON") 
-      num_files += 1
+      if (replace_in_files(file,"SET ANSI PADDING OFF","SET ANSI PADDING ON")):
+         num_files_modified +=1 
 
-   print(str(num_files) + " file(s) scanned and/or modified.") #make this smarter
+   print(str(num_files_modified) + " file(s) fixed.") 
   
 
 
