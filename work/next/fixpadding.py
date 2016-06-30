e50d15d13826c7a22bfdd872a274e75c29f9823b
Alex Narayan <alex@alexandarnarayan.com>
2016-06-29 23:25:54 -0700

    cleaning up and polishing it off

--- work/next/fixpadding.py
+++ work/next/fixpadding.py
@@ -1,11 +1,8 @@
 """
-Usage: FixPadding [-h | --help] [-q | --quiet] [-v | --verbose] [--extension FILEXTS] (INPUT ...)
+Usage: FixPadding [-h | --help] (INPUT ...)
 
 Options:
 -h --help                Show this
--q --quiet               Show no output, will always show number of files modified
--v --verbose             Print out filenames that were modified
---extension FILEXTS      Extensions to return, omitting means all valid files will be processed
 INPUT                    FILES to edit or DIRECTORIES to search recursively for files to edit
 
 """
@@ -30,26 +27,33 @@ def files_from_input(files):
 def combine_iterators(*args):
    yield from itertools.chain(args)
   
-def has_extension(**kwargs):
+
+def replace_in_file(file_to_edit,replace_this,with_this):
+   with open(file_to_edit,'r') as curr_file:
+      contents = curr_file.read()
+      contents = contents.replace(replace_this,with_this)
+   
+   with open(file_to_edit,'w+') as curr_file:
+      curr_file.write(contents)  
 
 
 if __name__ == '__main__':
    args = docopt(__doc__)
-   quiet      = args['--quiet']
-   verbose    = args['--verbose']
-   exts       = args['--extension']
    user_input = args['INPUT']
-   print(args)
   
    dirs           = files_by_dir(user_input)
    files_from_cli = files_from_input(user_input)
    
   
-
-   files = ( file for file in itertools.chain(dirs,files_from_cli) 
+   files             = itertools.chain(dirs,files_from_cli)
+   replication_files = (file for file in files if os.path.splitext(file)[1] == '.sch')
    
+   num_files = 0
+   for file in replication_files:
+      replace_in_file(file,"SET ANSI PADDING OFF","SET ANSI PADDING ON") 
+      num_files += 1
 
-
-
+   print(str(num_files) + " file(s) scanned and/or modified.") #make this smarter
+  
 
 
